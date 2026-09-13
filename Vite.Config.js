068c2d088ab6a31534eca73542d489f8490f6282
izankhalissani39 <izankhalissani39@gmail.com>
2026-09-13 21:16:37 +0700

    Update and rename Vite.Config.js to vite.config.js

--- Vite.Config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc' // atau @vitejs/plugin-react
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Pos-kasir/', // Sesuaikan dengan nama repositori Anda di GitHub
-})
